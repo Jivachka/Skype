a3ae878bf225993222617334977d0c7a9e6af831
AleksanderYa <250000ye@gmail.com>
2022-11-11 18:25:18 +0200

    Save file

--- work.py
+++ work.py
@@ -1,9 +1,14 @@
 from data import login_name, password, friend
 from skpy import Skype
+import os.path
 
 
 sk =  Skype(login_name, password)   # connect to Skype
 print(sk.user)
 
 ch = sk.chats['8:'+friend]
-print(ch.getMsgs())
+msg = ch.getMsgs()[0]
+
+
+with open(os.path.join("", msg.file.name), "wb") as f:
+    f.write(msg.fileContent) # Write the file to disk.
