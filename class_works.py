1bdfd68cdea6c27f4df81e07c0bd532f4ab5755d
Aleks <ya@gmail.com>
2023-04-02 13:07:09 +0300

    chanchge class_works.py and work.py

--- class_works.py
+++ class_works.py
@@ -1,24 +1,71 @@
-from data import login_name, password, friend
-from skpy import Skype
-from skpy import SkypeEventLoop
+import logging
+from data import login_name, password, allowed_friends
+from skpy import Skype, SkypeFileMsg
+import os.path
+import time
+from threading import Thread, Lock
 
-class LoginToSkype(object):
-    def __init__(self):
-        self._sk = Skype(login_name, password)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    def __enter__(self):
-        return self._sk
+class SkypeFileManager:
+    def __init__(self, username, password):
+        self.skype = Skype(username, password)
+        self.last_downloaded_file = None
+        self.last_downloaded_file_lock = Lock()
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+    def is_allowed_friend(self, friend_id):
+        return friend_id in allowed_friends
 
-class MySkype (SkypeEventLoop):
-    def onEvent(self, event):
-        if event.type == 'NewMessage':
-            print('New NMes')
-            return 'New ess'
+    def save_file(self, file_msg):
+        with open(os.path.join("Documents/", file_msg.file.name), "wb") as f:
+            f.write(file_msg.fileContent)
 
-if __name__ == '__main__':
-    sk = MySkype(login_name, password)
-    print(type(sk))
-    sk.cycle()
+    def get_file_name(self, file_msg):
+        if "." in file_msg.file.name:
+            return os.path.splitext(file_msg.file.name)[0]
+        else:
+            return file_msg.file.name
+
+    def send_response(self, chat, file_name):
+        chat.sendMsg(f"✅ File saved: (1f4be_floppydisk) \n {file_name} ")
+
+    def handle_message(self, event):
+        msg = event.msg
+        friend_id = msg.user.id
+        ch = msg.chat
+
+        if isinstance(msg, SkypeFileMsg) and self.is_allowed_friend(friend_id):
+            self.save_file(msg)
+            logging.info("New message received: %s", msg.content)
+            file_name = self.get_file_name(msg)
+            self.send_response(ch, file_name)
+            return file_name
+        return None
+
+    def start_thread(self):
+        while True:
+            events = self.skype.getEvents()
+            for event in events:
+                if event.type == "NewMessage":
+                    file_name = self.handle_message(event)
+                    if file_name:
+                        with self.last_downloaded_file_lock:
+                            self.last_downloaded_file = file_name
+            time.sleep(1)
+
+    def print_last_file_name(self):
+        while True:
+            with self.last_downloaded_file_lock:
+                logging.info("Last downloaded file: %s", self.last_downloaded_file)
+            time.sleep(5)
+
+file_manager = SkypeFileManager(login_name, password)
+
+num_threads = 5
+threads = [Thread(target=file_manager.start_thread) for _ in range(num_threads - 1)]
+threads.append(Thread(target=file_manager.print_last_file_name))
+for t in threads:
+    t.start()
+
+for t in threads:
+    t.join()
--- work.py
+++ work.py
@@ -1,6 +1,6 @@
 import logging
-from data import login_name, password, friend
-from skpy import Skype, SkypeFileMsg
+from data import login_name, password, allowed_friends
+from skpy import Skype, SkypeFileMsg, SkypeNewMessageEvent
 import os.path
 import time
 from threading import Thread, Lock
@@ -10,28 +10,37 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 # Set up the Skype connection
 sk = Skype(login_name, password)
-ch = sk.chats['8:' + friend]
 
 # Define a function to handle new messages
+def is_allowed_friend(friend_id):
+    return friend_id in allowed_friends
+
+def save_file(file_msg):
+    with open(os.path.join("Documents/", file_msg.file.name), "wb") as f:
+        f.write(file_msg.fileContent)  # Write the file to disk.
+
+def get_file_name(file_msg):
+    if "." in file_msg.file.name:
+        return os.path.splitext(file_msg.file.name)[0]
+    else:
+        return file_msg.file.name
+
+def send_response(chat, file_name):
+    chat.sendMsg(f"(1f4be_floppydisk) File saved: {file_name} ✅")
+
 def handle_message(event):
     msg = event.msg
-    if msg.chat == ch and isinstance(msg, SkypeFileMsg):
-        with open(os.path.join("Documents/", msg.file.name), "wb") as f:
-            f.write(msg.fileContent)  # Write the file to disk.
+    friend_id = msg.user.id
+    ch = msg.chat
+
+    if isinstance(msg, SkypeFileMsg) and is_allowed_friend(friend_id):
+        save_file(msg)
         logging.info("New message received: %s", msg.content)
-
-        # Check if the file name contains an extension
-        if "." in msg.file.name:
-            # Get the file name without the extension
-            file_name = os.path.splitext(msg.file.name)[0]
-        else:
-            file_name = msg.file.name
-
-        # Send a response in Skype
-        ch.sendMsg(f"(1f4be_floppydisk) File saved: {file_name} ✅")
-
+        file_name = get_file_name(msg)
+        send_response(ch, file_name)
         return file_name
     return None
+
 
 last_downloaded_file = None
 last_downloaded_file_lock = Lock()
@@ -51,7 +60,7 @@ def start_thread():
         # Check for new events
         events = sk.getEvents()
         for event in events:
-            if event.type == "NewMessage":
+            if isinstance(event, SkypeNewMessageEvent):
                 file_name = handle_message(event)
                 if file_name:
                     with last_downloaded_file_lock:
